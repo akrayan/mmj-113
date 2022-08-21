7a55668e7aee4dbb534d2d8f5d17ffd08ff6b8d3
Raihane AKKACHE <raihane.akkache@gmail.com>
2022-08-21 05:04:05 +0200

    Utils : timer v1

---
+++ Assets/Scripts/Utils/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+
+[Serializable]
+public class Timer
+{
+    [SerializeField][Min(0)]
+    private float _seconds = 0;
+    [SerializeField] bool _trueOnStart = true;
+    private float _timer;
+    private bool _init = false;
+
+    public void Start()
+    {
+        _timer = Time.time;
+        _init = true;
+    }
+
+    public void Restart()
+    {
+        _timer = Time.time;
+    }
+
+    public bool IsDone()
+    {
+        if (!_init)
+        {
+            Start();
+            return _trueOnStart;
+        }
+        return Time.time - _timer >= _seconds;
+    }
+
+    public bool IsDoneAndRestart()
+    {
+        if (!_init)
+        {
+            Start();
+            return _trueOnStart;
+        }
+        if (Time.time - _timer >= _seconds)
+        {
+            Restart();
+            return true;
+        }
+        return false;
+    }
+
+}
